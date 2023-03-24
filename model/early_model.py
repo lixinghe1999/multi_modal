d2a24d526e302a341f75cf3f350b353658fe97b9
lixinghe1999 <helixing99@gmail.com>
2023-03-24 09:07:10 +0800

    get to work on CNN

--- model/early_model.py
+++ model/early_model.py
@@ -71,7 +71,7 @@ class AVnet_Early(nn.Module):
     def forward(self, audio, image):
         if self.model == 'resnet':
             print(audio.shape, image.shape)
-            audio = torch.nn.functional.interpolate(audio, (-1, 3, 224, 224))
+            audio = torch.nn.functional.interpolate(audio, (224, 224))
             x = torch.cat([audio, image], dim=1)
             x = self.net.preprocess(x)
             for i, blk in enumerate(self.net.blocks):
