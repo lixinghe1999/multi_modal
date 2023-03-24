0b8e263d382b00a24bc20b64369782a876203d2d
lixinghe1999 <helixing99@gmail.com>
2023-03-24 15:27:20 +0800

    work on train supernet

--- channel_pruning.py
+++ channel_pruning.py
@@ -3,6 +3,7 @@ from utils.datasets.vggsound import VGGSound
 import numpy as np
 import torch
 from model.slim_model import AVnet_Slim
+from model.vanilla_model import AVnet
 import argparse
 import warnings
 from tqdm import tqdm
@@ -82,7 +83,8 @@ if __name__ == "__main__":
     batch_size = args.batch
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     torch.cuda.set_device(0)
-    model = AVnet_Slim().to('cuda')
+    # model = AVnet_Slim().to('cuda')
+    model = AVnet(model='resnet', pretrained=False).to(device)
     model.load_state_dict(torch.load('vanilla_resnet_AV_19_0.65678066.pth'), strict=False)
     dataset = VGGSound()
     len_train = int(len(dataset) * 0.8)
