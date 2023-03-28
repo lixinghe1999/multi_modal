e84079a5d43653f0c510b86fee1bf1145aed47a3
lixinghe1999 <helixing99@gmail.com>
2023-03-28 20:14:39 +0800

    work on train supernet

--- utils/datasets/vggsound.py
+++ utils/datasets/vggsound.py
@@ -67,7 +67,7 @@ class VGGSound(td.Dataset):
         # mean -4.268 and std 4.569 AST
         # mean -5.1991 and std 4.4049
         fbank = (fbank - (-5.1991)) / 4.4049
-        fbank = fbank.unsqueeze(0).expand(3, -1, -1)
+        # fbank = fbank.unsqueeze(0).expand(3, -1, -1)
         return fbank
     def __getitem__(self, index: int):
         sample = self.data[index]
