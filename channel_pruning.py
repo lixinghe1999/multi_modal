c51bc0aad474dfca1219ae11086beb35e3d60210
lixinghe1999 <helixing99@gmail.com>
2023-03-28 10:11:19 +0800

    work on train supernet

--- channel_pruning.py
+++ channel_pruning.py
@@ -147,7 +147,7 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     torch.cuda.set_device(0)
     model = AVnet_Slim(channel_split=16).to('cuda')
-    model.load_state_dict(torch.load('vanilla_resnet_AV_7_0.65220517.pth'))
+    model.load_state_dict(torch.load('vanilla_resnet_AV_7_0.65220517.pth'), strict=False)
     # model_distill = AVnet(model='resnet', pretrained=False).to(device)
     # model_distill.load_state_dict(torch.load('vanilla_resnet_AV_19_0.65678066.pth'), strict=False)
     # model_distill.load_state_dict(torch.load('slim_resnet_distill_9_0.5563375.pth'), strict=False)
