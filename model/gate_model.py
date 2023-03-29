760706fa5123e080baae595160946855cae3dafe
lixinghe1999 <helixing99@gmail.com>
2023-03-29 12:07:51 +0800

    work on train supernet

--- model/gate_model.py
+++ model/gate_model.py
@@ -165,8 +165,11 @@ class AVnet_Gate(nn.Module):
         output = self.head(feature)
 
         computation_penalty = torch.range(1, 12).to('cuda')/12
+        print(gate_a, gate_i)
         loss_c = ((gate_a * computation_penalty + gate_i * computation_penalty).mean()) ** 2
+        print(loss_c.item())
         loss_c += ((gate_a * computation_penalty).mean() - (gate_i * computation_penalty).mean()) ** 2
+        print(loss_c.item())
 
         loss_r = nn.functional.cross_entropy(output, label) # recognition-level loss
 
