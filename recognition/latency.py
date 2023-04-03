b3914d87a026c1206ba5a76839956d69c1619c35
lixinghe1999 <helixing99@gmail.com>
2023-04-03 20:08:02 +0800

    implement without timm

--- recognition/latency.py
+++ recognition/latency.py
@@ -2,7 +2,7 @@ import torch
 from model.dynamicvit_legacy import AVnet_Dynamic
 import time
 import argparse
-from fvcore.nn import FlopCountAnalysis, parameter_count_table
+
 import numpy as np
 def rfft_flop_jit(inputs, outputs):
     """
@@ -15,6 +15,7 @@ def rfft_flop_jit(inputs, outputs):
     return flops
 
 def calc_flops(model, input, show_details=False, ratios=None):
+    from fvcore.nn import FlopCountAnalysis, parameter_count_table
     model.eval()
     with torch.no_grad():
         model.default_ratio = ratios
@@ -66,7 +67,7 @@ if __name__ == "__main__":
 
     model = AVnet_Dynamic(pruning_loc=pruning_loc, token_ratio=token_ratio, pretrained=False).to(device)
     model.eval()
-    # throughput(model, (audio, image))
+    throughput(model, (audio, image))
     # calc_flops(model, (audio, image), show_details=False)
     # torch.save(model.state_dict(), 'dynamic_token.pth')
     # torch.onnx.export(model, (audio, image), 'dynamic.onnx', input_names=['input_1', 'input_2'],
@@ -77,13 +78,13 @@ if __name__ == "__main__":
     # onnx.checker.check_model(onnx_model)
 
 
-    import onnxruntime
-    torch_out = model(audio, image)
-    ort_session = onnxruntime.InferenceSession("dynamic.onnx")
-    ort_outs = ort_session.run(None,  {
-    "input_1": audio.detach().cpu().numpy().astype(np.float32),
-    "input_2": image.detach().cpu().numpy().astype(np.float32)
-    })
-    # compare ONNX Runtime and PyTorch results
-    np.testing.assert_allclose(torch_out.detach().cpu().numpy(), ort_outs[0], rtol=1e-02, atol=1e-05)
-    print("Exported model has been tested with ONNXRuntime, and the result looks good!")
+    # import onnxruntime
+    # torch_out = model(audio, image)
+    # ort_session = onnxruntime.InferenceSession("dynamic.onnx")
+    # ort_outs = ort_session.run(None,  {
+    # "input_1": audio.detach().cpu().numpy().astype(np.float32),
+    # "input_2": image.detach().cpu().numpy().astype(np.float32)
+    # })
+    # # compare ONNX Runtime and PyTorch results
+    # np.testing.assert_allclose(torch_out.detach().cpu().numpy(), ort_outs[0], rtol=1e-02, atol=1e-05)
+    # print("Exported model has been tested with ONNXRuntime, and the result looks good!")
