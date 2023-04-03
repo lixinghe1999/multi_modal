3cf5bdaedf7a4364c2450116fa155e5e2e076086
lixinghe1999 <helixing99@gmail.com>
2023-04-03 20:29:08 +0800

    implement without timm

--- recognition/model/dynamicvit_legacy.py
+++ recognition/model/dynamicvit_legacy.py
@@ -10,9 +10,9 @@ class AVnet_Dynamic(nn.Module):
     def __init__(self, distill=False, \
                  pruning_loc=[3, 6, 9], token_ratio=[0.7, 0.7 ** 2, 0.7 ** 3], pretrained=True, threshold=0.99):
         super(AVnet_Dynamic, self).__init__()
-        config = dict(patch_size=16, embed_dim=768, depth=2, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        embed_dim = 384
+        config = dict(patch_size=16, embed_dim=embed_dim, depth=12, num_heads=12, mlp_ratio=1, qkv_bias=True,
                       pruning_loc=pruning_loc, token_ratio=token_ratio)
-        embed_dim = 768
         self.audio = AudioTransformerDiffPruning(config, imagenet_pretrain=pretrained)
         self.audio.score_predictor = None
         self.audio.head = None
