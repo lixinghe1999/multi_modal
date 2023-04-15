fcc4c00d4df81a622cd95e1f5c9894536eceb60a
lixinghe1999 <helixing99@gmail.com>
2023-04-15 18:54:43 +0800

    work on har

--- classification/har/model/vanilla_model.py
+++ classification/har/model/vanilla_model.py
@@ -46,7 +46,7 @@ class HARnet(nn.Module):
         # image, imu, radar
         self.branch = nn.ModuleList()
         self.branch.append(AdaConvNeXt(in_chans=16, sparse_ratio=[0.8, 0.6, 0.4], pruning_loc=[3,6,9], num_classes=10, depths=[3, 3, 27, 3]))
-        self.branch.append(AdaConvNeXt(in_chans=16, sparse_ratio=[0.8, 0.6, 0.4], pruning_loc=[3,6,9], num_classes=10, depths=[3, 3, 27, 3]))
+        self.branch.append(AdaConvNeXt(in_chans=960, sparse_ratio=[0.8, 0.6, 0.4], pruning_loc=[3,6,9], num_classes=10, depths=[3, 3, 27, 3]))
         self.branch.append(AdaConvNeXt(sparse_ratio=[0.8, 0.6, 0.4], pruning_loc=[3,6,9], num_classes=10, depths=[3, 3, 27, 3]))
 
         if pretrained:
