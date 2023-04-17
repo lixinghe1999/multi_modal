356ab6d6b92cb3f333e522b5da5ea53a6befb4ae
lixinghe1999 <helixing99@gmail.com>
2023-04-17 18:16:42 +0800

    without dependency on tf

--- segmentation/src/datasets/__init__.py
+++ segmentation/src/datasets/__init__.py
@@ -2,7 +2,7 @@
 """
 .. codeauthor:: Daniel Seichter <daniel.seichter@tu-ilmenau.de>
 """
-from .cityscapes.pytorch_dataset import Cityscapes
-from .nyuv2.pytorch_dataset import NYUv2
+# from .cityscapes.pytorch_dataset import Cityscapes
+# from .nyuv2.pytorch_dataset import NYUv2
 from .sunrgbd.pytorch_dataset import SUNRGBD
-from .scenenetrgbd.pytorch_dataset import SceneNetRGBD
+# from .scenenetrgbd.pytorch_dataset import SceneNetRGBD
