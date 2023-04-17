d784fd21beb6def42dbdf1db232c040dc2b44f88
lixinghe1999 <helixing99@gmail.com>
2023-04-17 19:06:11 +0800

    without dependency on tf

--- segmentation/eval.py
+++ segmentation/eval.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     cameras = data_loader.dataset.cameras
 
     for camera in cameras:
-        print('working on ', camera)
+        print('working on', camera)
         confusion_matrices[camera] = dict()
         confusion_matrices[camera] = ConfusionMatrixPytorch(n_classes)
         n_samples_total = len(data_loader.dataset)
@@ -100,6 +100,7 @@ if __name__ == '__main__':
 
                     # confusion_matrices[camera].update_conf_matrix(label, pred)
                     confusion_matrices[camera].update(torch.from_numpy(label), torch.from_numpy(pred))
+                    print(confusion_matrices[camera]._num_examples)
                 print(f'\r{i + 1}/{len(data_loader)}', end='')
 
         # miou, _ = confusion_matrices[camera].compute_miou()
