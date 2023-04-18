46fc0daf283f97a453d21b0ad5b8200a3a166515
lixinghe1999 <helixing99@gmail.com>
2023-04-18 17:51:48 +0800

    work on training

--- classification/vanilla.py
+++ classification/vanilla.py
@@ -78,7 +78,7 @@ def train(model, train_dataset, test_dataset):
 
                 one_hot_label = torch.nn.functional.one_hot(batch[-1].to(device), num_classes=309)
                 r = ((model.modality_weight[0] * one_hot_label).sum(dim=-1) / (
-                            model.modality_weight[1] * one_hot_label).sum(dim=-1)).mean()
+                            model.modality_weight[1] * one_hot_label).sum(dim=-1)).mean().item()
                 ratio.append(r)
         import pickle
         file = open(r"modality_weight.pkl", "wb")
