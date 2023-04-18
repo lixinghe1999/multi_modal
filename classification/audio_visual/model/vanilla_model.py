f40737b59220979f232d8161428caeee6ffddd45
lixinghe1999 <helixing99@gmail.com>
2023-04-18 16:52:05 +0800

    work on training

--- classification/audio_visual/model/vanilla_model.py
+++ classification/audio_visual/model/vanilla_model.py
@@ -70,6 +70,7 @@ class AVnet(nn.Module):
         x = torch.cat([audio[:, 0], image[:, 0]], dim=1)
         x = torch.flatten(x, start_dim=1)
         print(x.shape)
+        print(self.head[0].weight.shape)
         self.modality_weight = [nn.functional.linear(x[:, :self.embed_dim], self.head[0].weight[:self.embed_dim],
                                                      self.head[0].bias[:self.embed_dim]),
                                 nn.functional.linear(x[:, self.embed_dim:], self.head[0].weight[self.embed_dim:],
