e0cb7ac626ecbd57c5adc4646a977bc5854260a1
lixinghe1999 <helixing99@gmail.com>
2023-04-18 19:11:05 +0800

    work on training

--- segmentation/src/utils.py
+++ segmentation/src/utils.py
@@ -38,7 +38,7 @@ class CrossEntropyLoss2d(nn.Module):
             targets_m = targets.clone()
             targets_m -= 1
             # loss_all = self.ce_loss(inputs, targets_m.long())
-            loss_all = self.ce_loss(inputs.long(), targets_m.long())
+            loss_all = self.ce_loss(inputs.float(), targets_m.long())
             # loss_all = torch.nn.functional.cross_entropy(inputs, targets_m)
             number_of_pixels_per_class = \
                 torch.bincount(targets.flatten().type(self.dtype),
