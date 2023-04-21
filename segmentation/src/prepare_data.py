e8949a601c1efc214009f316c8fcf3cd6515ea8a
lixinghe1999 <helixing99@gmail.com>
2023-04-21 19:03:36 +0800

    work on teacher model

--- segmentation/src/prepare_data.py
+++ segmentation/src/prepare_data.py
@@ -9,7 +9,7 @@ import pickle
 from torch.utils.data import DataLoader
 
 from src import preprocessing
-# from src.datasets import Cityscapes
+from src.datasets import Cityscapes
 from src.datasets import NYUv2
 # from src.datasets import SceneNetRGBD
 from src.datasets import SUNRGBD
