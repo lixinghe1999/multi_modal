98909a9574bb38371c747e48b44c29e7a98b11ed
lixinghe1999 <helixing99@gmail.com>
2023-04-21 15:02:29 +0800

    work on mobilenet

--- segmentation/src/models/fastseg/lraspp.py
+++ segmentation/src/models/fastseg/lraspp.py
@@ -101,7 +101,10 @@ class LRASPP(BaseSegmentation):
         y = self.conv_up3(y)
         y = self.last(y)
         y = F.interpolate(y, size=x.shape[2:], mode='bilinear', align_corners=False)
-        return [y]
+        if self.training:
+            return [y]
+        else:
+            return y
 
 
 class MobileV3Large(LRASPP):
