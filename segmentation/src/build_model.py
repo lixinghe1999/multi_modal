7328542f73c5b5c7a0a26c3efe662c47c5724193
lixinghe1999 <helixing99@gmail.com>
2023-04-21 18:05:47 +0800

    work on teacher model

--- segmentation/src/build_model.py
+++ segmentation/src/build_model.py
@@ -47,6 +47,7 @@ def build_model(args, n_classes):
         # specific depth encoder is provided
         if args.encoder_depth in [None, 'None']:
             args.encoder_depth = args.encoder
+        print(args.network)
         if args.network == 'resnet':
             model = ESANet(
                 height=args.height,
