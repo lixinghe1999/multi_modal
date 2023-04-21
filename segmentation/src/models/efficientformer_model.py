8a372763aba704dd13472e287f3b4576344efebd
lixinghe1999 <helixing99@gmail.com>
2023-04-21 09:09:36 +0800

    update efficient former

--- segmentation/src/models/efficientformer_model.py
+++ segmentation/src/models/efficientformer_model.py
@@ -97,15 +97,13 @@ class DynamicRGBD(nn.Module):
         if pretrained_on_imagenet:
             print('load the pretrained model')
             # load imagenet pretrained or segmentation pretrained
-            weight = torch.load('../assets/upernet_convnext_small_1k_512x512.pth')['state_dict']
+            weight = torch.load('../assets/eformer_s0_450.pth')['state_dict']
             weight_backbone = {k[9:]: v for k, v in weight.items() if k.split('.')[0] == 'backbone'}
             # weight = torch.load('../assets/convnext_small_1k_224.pth')['model']
             # weight = {k: v for k, v in weight.items() if k.split('.')[0] != 'head'}
             self.encoder_rgb.load_state_dict(weight_backbone)
             self.encoder_depth.load_state_dict(weight_backbone)
-            weight_uperhead = {k[12:]: v for k, v in weight.items() if k.split('.')[0] == 'decode_head'}
-            weight_uperhead = {k: v for k, v in weight_uperhead.items() if k.split('.')[0] != 'conv_seg'}
-            self.UPerHead.load_state_dict(weight_uperhead, strict=False)
+
 
 
 
