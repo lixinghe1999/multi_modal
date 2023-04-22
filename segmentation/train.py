052041f23f1ec444f1eb873abc6913d72e4ce895
lixinghe1999 <helixing99@gmail.com>
2023-04-22 08:45:28 +0800

    work on teacher model

--- segmentation/train.py
+++ segmentation/train.py
@@ -96,11 +96,6 @@ def train_main():
 
     # loss, optimizer, learning rate scheduler, csvlogger  ----------
 
-    # setattr(args, 'network', 'resnet')
-    # setattr(args, 'pretrained_dir', '../assets/r34_NBt1D.pth')
-    # teacher_model, _ = build_model(args, n_classes=n_classes_without_void)
-    # for j, p in enumerate(teacher_model.parameters()):
-    #     p.requires_grad_(False)
     loss_function_train = utils.CrossEntropyLoss2d(device=device, weight=class_weighting)
 
     pixel_sum_valid_data = valid_loader.dataset.compute_class_weights(
