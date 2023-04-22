e275e22fd120565b659778a4313a240f8a5d322a
lixinghe1999 <helixing99@gmail.com>
2023-04-22 09:05:12 +0800

    work on teacher model

--- segmentation/src/build_model.py
+++ segmentation/src/build_model.py
@@ -119,11 +119,10 @@ def build_model(args, n_classes):
                 upsampling=args.upsampling
             )
         else:
-            model = MobileV3Large.from_pretrained()
-            # if args.pretrained_on_imagenet:
-            #     model = MobileV3Large.from_pretrained()
-            # else:
-            #     model = MobileV3Large(num_classes=n_classes)
+            if args.pretrained_on_imagenet:
+                model = MobileV3Large.from_pretrained()
+            else:
+                model = MobileV3Large(num_classes=n_classes)
 
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
