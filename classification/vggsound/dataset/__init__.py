c2abd6af645000feb8122503a1add2a0d6268bdd
lixinghe1999 <helixing99@gmail.com>
2023-05-18 14:28:39 +0800

    remove the older name

--- classification/vggsound/dataset/__init__.py
+++
@@ -1,2 +0,0 @@
-from .vggsound import VGGSound
-from .epic_kitchen import EPICKitchen, get_test_transform, get_train_transform
--- classification/vggsound/dataset/dataset_split.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-train_list = pd.read_pickle('dataset/epic_kitchen_100/EPIC_train.pkl')
-test_list = pd.read_pickle('dataset/epic_kitchen_100/EPIC_val.pkl')
-participants = ['P01', 'P02', 'P03', 'P04', 'P06', 'P07']
-new_train_list = train_list[train_list['participant_id'].isin(participants)]
-new_test_list = test_list[test_list['participant_id'].isin(participants)]
-print(len(new_train_list), len(new_test_list))
-new_train_list.to_pickle('EPIC_train.pkl')
-new_test_list.to_pickle('EPIC_val.pkl')
