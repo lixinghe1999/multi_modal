fabb0c6906a5b726b95906feebd0eeb5a99a0bfe
lixinghe1999 <helixing99@gmail.com>
2023-06-07 09:14:12 +0800

    split retrieval

---
+++ classification/event_retrieval/dataset/dataset_split.py
@@ -0,0 +1,35 @@
+import pandas as pd 
+import argparse
+import h5py
+import random
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('--mode', type=str, default='label')
+    args = parser.parse_args()
+    participants = ['P01', 'P02', 'P03', 'P04', 'P05', 'P06']
+    if args.mode == 'label':
+        train_list = pd.read_pickle('dataset/epic_kitchen_100/EPIC_train.pkl')
+        test_list = pd.read_pickle('dataset/epic_kitchen_100/EPIC_val.pkl')
+        # just split the original train and test list
+        new_train_list = train_list[train_list['participant_id'].isin(participants)]
+        new_test_list = test_list[test_list['participant_id'].isin(participants)]
+        print(len(new_train_list), len(new_test_list))
+
+        # split the sub-dataset independently
+        new_data_list = pd.concat([new_train_list, new_test_list])
+        new_train_list = new_data_list.sample(frac=0.8, random_state=42)
+        new_test_list = new_data_list.drop(new_train_list.index)
+        print(len(new_train_list), len(new_test_list))
+
+        new_train_list.to_pickle('EPIC_train.pkl')
+        new_test_list.to_pickle('EPIC_val.pkl')
+    else: # for audio split
+        audio = h5py.File('../EPIC_audio.hdf5', 'r')
+        keys_list = list(audio.keys())
+        filtered_list = [keys_list[i] for i in range(len(keys_list)) if keys_list[i].split('_')[0] in participants]
+        print(len(filtered_list), filtered_list)
+
+        new_audio = h5py.File('../split_EPIC_audio.hdf5', 'w')
+        for key in filtered_list:
+            new_audio.create_dataset(key, data=audio[key])
---
+++ classification/event_retrieval/models/sft.py
@@ -0,0 +1,72 @@
+'''
+backbone:
+just late fusion
+'''
+import torch.nn as nn
+import torch
+from torch.cuda.amp import autocast
+from .vit_model import AudioTransformer, VisionTransformer, Block
+
+class SFT(nn.Module):
+    def __init__(self, scale='base', pretrained=False):
+        super(SFT, self).__init__()
+        if scale == 'base':
+            config = dict(patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True, pruning_loc=())
+            pretrained_weight = torch.load('pretrained/deit_base_patch16_224.pth')['model']
+            self.embed_dim = 768
+        elif scale == 'small':
+            config = dict(patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True, pruning_loc=())
+            pretrained_weight = torch.load('pretrained/deit_small_patch16_224.pth')['model']
+            self.embed_dim = 384
+        else:
+            config = dict(patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True, pruning_loc=())
+            pretrained_weight = torch.load('pretrained/deit_tiny_patch16_224.pth')['model']
+            self.embed_dim = 192
+
+        self.audio = AudioTransformerDiffPruning(config, pretrained=pretrained_weight)
+        self.audio.head = None
+        self.image = VisionTransformerDiffPruning(**config)
+        if pretrained:
+            self.image.load_state_dict(pretrained_weight, strict=False)
+        self.image.head = None
+
+        self.depth_clip = 9
+        self.len_blocks = len(self.audio.blocks)
+        self.image.blocks = self.image.blocks[:self.depth_clip]
+        self.audio.blocks = self.audio.blocks[:self.depth_clip]
+        self.sparse_blocks = nn.ModuleList([
+            Block(dim=self.embed_dim, num_heads=config['num_heads'], mlp_ratio=config['mlp_ratio'], qkv_bias=config['qkv_bias'])
+            for _ in range(self.len_blocks - self.depth_clip)])
+
+        self.head = nn.Linear(self.embed_dim * 2, 309)
+        self.modality_weight = []
+    def fusion_parameter(self):
+        parameter = [{'params': self.head.parameters()},
+        ]
+        return parameter
+
+    @autocast()
+    def forward(self, audio, image):
+        self.modality_weight = []
+        B, audio = self.audio.preprocess(audio.unsqueeze(1))
+        B, image = self.image.preprocess(image)
+
+        for i, (blk_a, blk_i) in enumerate(zip(self.audio.blocks, self.image.blocks)):
+            audio = blk_a(audio)
+            image = blk_i(image)
+
+        # sparse
+        x = torch.stack([audio[:, 0], image[:, 0], audio[:, 1:].mean(dim=1), image[:, 1:].mean(dim=1)], dim=2).permute(0,2,1)
+
+        # sparse encoding
+        for i, blk in enumerate(self.sparse_blocks):
+            x = blk(x)
+        x = self.audio.norm(x)
+        x = torch.cat([x[:, 0], x[:, 1]], dim=1)
+        x = torch.flatten(x, start_dim=1)
+        self.modality_weight = [nn.functional.linear(x[:, :self.embed_dim], self.head.weight[:, :self.embed_dim],
+                                                     self.head.bias/2),
+                                nn.functional.linear(x[:, self.embed_dim:], self.head.weight[:, self.embed_dim:],
+                                                     self.head.bias/2)]
+        x = self.head(x)
+        return x
